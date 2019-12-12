61275668f3f4c85563eae01db78a4b6cdc077229
Bruno Cascio <bruno.cascio@celerative.com>
2019-12-12 15:44:34 -0300

    hotfix multiline vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ set -e o pipefail
 
 env_variables=$(jq -n 'env' | jq --arg CONTAINER "$CONTAINER_NAME" -r 'keys[] as $k | "-e \($CONTAINER) \($k) \"\(values[$k])\""')
 
-eval "ecs deploy $CLUSTER_NAME $SERVICE_NAME --task $TASK_NAME $env_variables --exclusive-env"
+command=$(printf "ecs deploy $CLUSTER_NAME $SERVICE_NAME --task $TASK_NAME $env_variables --exclusive-env")
+
+eval $command
