60e223d9be21b87b96c51b761d96992058fd75f4
Bruno Cascio <bruno.cascio@celerative.com>
2019-12-12 16:44:23 -0300

    logg

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,15 @@
 
 set -e o pipefail
 
+echo $INPUT_CONTAINER
+echo $INPUT_CLUSTER
+echo $INPUT_TASK
+echo $INPUT_SERVICE
+
 env_variables=$(jq -n 'env' | jq --arg CONTAINER "$INPUT_CONTAINER" -r 'keys[] as $k | "-e \($CONTAINER) \($k) \"\(values[$k])\""')
 
 command=$(printf "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --task $INPUT_TASK $env_variables --exclusive-env")
+
+echo $command
 
 eval $command
