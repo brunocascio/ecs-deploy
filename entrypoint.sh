24f07e7af6bb5ed0b79781e30a9cb312ad7902ea
Bruno Cascio <bruno.cascio@celerative.com>
2019-12-12 16:00:45 -0300

    inverted commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e o pipefail
 
 env_variables=$(jq -n 'env' | jq --arg CONTAINER "$CONTAINER_NAME" -r 'keys[] as $k | "-e \($CONTAINER) \($k) \"\(values[$k])\""')
 
-command=$(printf "ecs deploy $CLUSTER_NAME $SERVICE_NAME --task $TASK_NAME $env_variables --exclusive-env")
+command=$(printf "ecs deploy --task $TASK_NAME --exclusive-env $env_variables $CLUSTER_NAME $SERVICE_NAME")
 
 eval $command
