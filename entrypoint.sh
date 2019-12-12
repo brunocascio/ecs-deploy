a5973f9a56333af5990f16ebeba40c9659911d2a
Bruno Cascio <bruno.cascio@celerative.com>
2019-12-12 17:06:26 -0300

    docs and valdiations

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,17 @@
 
 set -e o pipefail
 
-echo $INPUT_CONTAINER
-echo $INPUT_CLUSTER
-echo $INPUT_TASK
-echo $INPUT_SERVICE
+# Validate input
+[ -z "$INPUT_CLUSTER" ] && (echo "Missing Cluster Name" && exit 1)
+[ -z "$INPUT_CONTAINER" ] && (echo "Missing Container Name" && exit 1)
+[ -z "$INPUT_TASK" ] && (echo "Missing Task Name" && exit 1)
+[ -z "$INPUT_SERVICE" ] && (echo "Missing Service Name" && exit 1)
 
+# parse env variables
 env_variables=$(jq -n 'env' | jq --arg CONTAINER "$INPUT_CONTAINER" -r 'keys[] as $k | "-e \($CONTAINER) \($k) \"\(values[$k])\""')
 
+# make string command to be evaluated (one line string)
 command=$(printf "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --task $INPUT_TASK $env_variables --exclusive-env")
 
-echo $command
-
+# fire command
 eval $command
