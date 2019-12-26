fe794c04d53e6691d74a1ded3d1de91ed01c42ab
Bruno Cascio <bruno.cascio@celerative.com>
2019-12-26 17:57:25 -0300

    added optional env file

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ This action deploys ECS services using [fabfuel/ecs-deploy](https://github.com/f
 ## Example usage
 
 ```yml
-uses: brunocascio/ecs-deploy@vX.Y.Z>
+uses: brunocascio/ecs-deploy@v1>
 with:
   cluster: theClusterName
   service: theServiceName
   task: theTaskDefinitionName
   container: theContainerName
+  envfile: /path/to/your/envfile (optionally)
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,16 @@ set -e o pipefail
 [ -z "$INPUT_TASK" ] && (echo "Missing Task Name" && exit 1)
 [ -z "$INPUT_SERVICE" ] && (echo "Missing Service Name" && exit 1)
 
-# parse env variables
-env_variables=$(jq -n 'env' | jq --arg CONTAINER "$INPUT_CONTAINER" -r 'keys[] as $k | "-e \($CONTAINER) \($k) \"\(values[$k])\""')
+if [ -z "$INPUT_ENVFILE" ]; 
+then
+    # parse system env variables
+    echo "Env variables are read from 'env' command"
+    env_variables=$(jq -n 'env' | jq --arg CONTAINER "$INPUT_CONTAINER" -r 'keys[] as $k | "-e \($CONTAINER) \($k) \"\(values[$k])\""')
+else
+    # parse file env variables
+    [ ! -f $INPUT_ENVFILE ] && echo "File $INPUT_ENVFILE not found" && exit 1
+    env_variables=$(cat "$INPUT_ENVFILE" | awk -F= -v CONTAINER="$INPUT_CONTAINER" '{print "-e " CONTAINER " " $1 " " "\"" $2 "\""}')
+fi
 
 # make string command to be evaluated (one line string)
 command=$(printf "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --task $INPUT_TASK $env_variables --exclusive-env")
