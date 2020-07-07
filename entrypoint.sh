b05feaf04098d61f9005457d8a45f94668e7a52c
Nadir Hamid <matrix.nad@gmail.com>
2020-07-07 15:54:53 -0600

    add region option

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ set -e o pipefail
 [ -z "$INPUT_CONTAINER" ] && (echo "Missing Container Name" && exit 1)
 [ -z "$INPUT_TASK" ] && (echo "Missing Task Name" && exit 1)
 [ -z "$INPUT_SERVICE" ] && (echo "Missing Service Name" && exit 1)
+[ -z "$INPUT_REGION" ] && (echo "Missing Region" && exit 1)
 
 TIMEOUT=${INPUT_TIMEOUT:-300}
 
@@ -15,9 +16,9 @@ then
     [ ! -f $INPUT_ENVFILE ] && echo "$INPUT_ENVFILE not found" && exit 1 
     env_variables=$(grep -v '^#' "$INPUT_ENVFILE" | grep . | sed -E 's/=/ /1' | awk -v CONTAINER="$INPUT_CONTAINER" '{print "-e " CONTAINER " " $1 " " $2}')
     # make string command to be evaluated (one line string)
-    command=$(echo -n "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --timeout $TIMEOUT --task $INPUT_TASK $env_variables --exclusive-env")
+    command=$(echo -n "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --region $INPUT_REGION --timeout $TIMEOUT --task $INPUT_TASK $env_variables --exclusive-env")
 else
-    command=$(echo -n "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --timeout $TIMEOUT --task $INPUT_TASK")
+    command=$(echo -n "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --region $INPUT_REGION --timeout $TIMEOUT --task $INPUT_TASK")
 fi
 
 # fire command
