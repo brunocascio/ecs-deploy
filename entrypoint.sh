098217863a0b321fd61530f14c02f63b69274761
Nadir Hamid <matrix.nad@gmail.com>
2020-07-07 16:37:27 -0600

    add AWS creds to action options

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ set -e o pipefail
 [ -z "$INPUT_TASK" ] && (echo "Missing Task Name" && exit 1)
 [ -z "$INPUT_SERVICE" ] && (echo "Missing Service Name" && exit 1)
 [ -z "$INPUT_REGION" ] && (echo "Missing Region" && exit 1)
+[ -z "$INPUT_ACCESS_KEY" ] && (echo "Missing Access Key" && exit 1)
+[ -z "$INPUT_SECRET_KEY" ] && (echo "Missing Access Key" && exit 1)
 
 TIMEOUT=${INPUT_TIMEOUT:-300}
 
@@ -18,7 +20,7 @@ then
     # make string command to be evaluated (one line string)
     command=$(echo -n "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --region $INPUT_REGION --timeout $TIMEOUT --task $INPUT_TASK $env_variables --exclusive-env")
 else
-    command=$(echo -n "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --region $INPUT_REGION --timeout $TIMEOUT --task $INPUT_TASK")
+    command=$(echo -n "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --access-key-id $INPUT_ACCESS_KEY --secret-access-key $INPUT_SECRET_KEY --region $INPUT_REGION --timeout $TIMEOUT --task $INPUT_TASK")
 fi
 
 # fire command
